f79b308ef785115d79f709774c79e0abacc2d802
Maja Repinc <maja.repinc96@gmail.com>
2019-04-14 13:45:32 +0200

    redid the window manipulation

--- js/window-modal.js
+++ js/window-modal.js
@@ -1,7 +1,39 @@
 // register modal component
-Vue.component("modal", {
-  template: "#window-modal-template",
-  props: ['el'],
+var windowModalComponent = Vue.component("modal", {
+  template:`
+  <transition name="modal" @after-enter="draggableResizable();">
+      <div class="modal-wrapper">
+          <div class="modal-container">
+              <div class='resizers'>
+                  <div class="modal-header">
+                      <slot name="header">
+                          default header
+                      </slot>
+                      <div class="window-buttons">
+                          <div class="window-expand-shrink" @click="expandShrink()">
+                              <div class="fas fa-expand" title="Expand"></div>
+                          </div>
+                          <div class="window-minimize">
+                              <div class="fas fa-minus-circle" title="Minimize"></div>
+                          </div>
+                          <div class="window-close">
+                              <div class="fas fa-times-circle" title="Close" @click="$emit('close')"></div>
+                          </div>
+                      </div>
+                  </div>
+                  <div style="clear:both"></div>
+
+                  <div class="modal-body">
+                      <slot name="body">
+                          default body
+                      </slot>
+                  </div>
+                  <div class='resizer bottom-right fas fa-caret-right'></div>
+              </div>
+          </div>
+      </div>
+  </transition>
+  `,
   methods: {
       draggableResizable: function () {
           funDraggable("#" + this.$el.parentNode.id + " .modal-wrapper");
@@ -12,22 +44,43 @@ Vue.component("modal", {
       }
   }
 
-})
+});
 
-// start app
-var about = new Vue({
-  el: "#icon-about",
-  data: {
-    showModal: false,
-    expand: false
-  }
-})
 
-// start app
-var settings = new Vue({
+// start about app
+var aboutAppButton = new Vue({
+    el: "#icon-about",
+    methods: {
+      showAboutWindow: function() {
+          console.log("clicked " + aboutAppWindow.name);
+          aboutAppWindow.showModal = true;
+      }
+    }
+});
+
+var aboutAppWindow = new Vue ({
+    el: "#about-modal",
+    data: {
+		name: "About Window",
+        showModal: false
+	}
+});
+
+// start settings app
+var settingsAppButton = new Vue({
   el: "#icon-settings",
-  data: {
-    showModal: false,
-    expand: false
+  methods: {
+    showAboutWindow: function() {
+        console.log("clicked " + settingsAppWindow.name)
+        settingsAppWindow.showModal = true;
+    }
   }
-})
+});
+
+var settingsAppWindow = new Vue ({
+    el: "#settings-modal",
+    data: {
+		name: "Settings Window",
+        showModal: false
+	}
+});
