293ddd069e81e0c7fd2b5f79464567304b1ee31b
Maja Repinc <maja.repinc96@gmail.com>
2019-05-11 09:47:57 +0100

    added loading, mobile check, drag on touch, resize on mobile

--- js/script.js
+++ js/script.js
@@ -1,20 +1,73 @@
 //call functions when page fully loads
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    funStartTime();
+
+    //create loading screen to fully load all content
+    loadingScreen(document.getElementById("loading-label"), 100);
+
+    setTimeout(function() { document.getElementById("loading").style.display = "none";}, 4000);
+
+    //start clock
+    startTime();
+
+    //enable listeners on hover
     changeHover();
 
     document.getElementById("icon-expand").onclick = function() {
-      funFullscreen();
+      triggerFullscreen();
     };
 
     document.addEventListener('keyup', function (event) {
       if (event.defaultPrevented) { return; }
       var key = event.key || event.keyCode;
 
-      if (key === 'F11') { funKeyFullscreen() }
+      if (key === 'F11' || key === "Escape") { keyFullscreen(key) }
     });
 
+    //check for mobile browser
+    window.mobilecheck = function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
+    if(window.mobilecheck()) {
+        document.querySelector("body").setAttribute("class","mobile-view")
+    }
+
+
 });
+
+
+function loadingScreen(elementClass, typingSpeed){
+    var cursor = document.createElement("div");
+    cursor.setAttribute("id", "loading-cursor")
+  var thhis = elementClass;
+  thhis.style.position =  "relative";
+  thhis.style.display =  "inline-block"
+
+  thhis.append(cursor);
+  var loadCursor = document.getElementById("loading-cursor");
+  thhis = document.getElementById("loading-text");
+  var text = thhis.innerText.trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.innerText = "";
+  var loaded = false;
+  setTimeout(function(){
+    thhis.style.opacity = 1;
+    thhis.innerText = "";
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {
+          newString += char;
+          thhis.innerText = newString;
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+
+
+}
 
 var themes = [
     {
@@ -45,11 +98,11 @@ var themes = [
 
 
 
-function funKeyFullscreen() {
+function keyFullscreen(key) {
     console.log("clicked f11");
     const fullScreenIcon =  document.getElementById("icon-expand").getElementsByTagName("i")[0].className;
 
-      if (fullScreenIcon === "fas fa-expand-arrows-alt") {
+      if (fullScreenIcon === "fas fa-expand-arrows-alt" && key !== "Escape") {
           document.getElementById("icon-expand").getElementsByTagName("i")[0].className = "fas fa-compress-arrows-alt";
           console.log("opened fullscreen");
       } else {
@@ -58,17 +111,18 @@ function funKeyFullscreen() {
       }
 }
 
-function funFullscreen() {
+function triggerFullscreen() {
+
     const isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
       (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
       (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
       (document.msFullscreenElement && document.msFullscreenElement !== null);
      if (!isInFullScreen) {
-         funOpenFullscreen();
+         openFullscreen();
          document.getElementById("icon-expand").getElementsByTagName("i")[0].className = "fas fa-compress-arrows-alt";
          console.log("opened fullscreen");
      } else {
-         funCloseFullscreen();
+         closeFullscreen();
          document.getElementById("icon-expand").getElementsByTagName("i")[0].className = "fas fa-expand-arrows-alt";
          console.log("closed fullscreen");
      }
@@ -86,21 +140,22 @@ function makeDraggable(id) {
     this.events = function () {
         var self = this;
         _on(element.getElementsByClassName('modal-header')[0], 'mousedown', function (e) {
-            //console.log("started dragging");
-            isDragReady = true;
-            e.preventDefault();
-            var modalMousedown = document.querySelectorAll(".modal-wrapper")
-            //crossbrowser mouse pointer values
-            e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-            e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-            dragoffset.x = e.pageX - element.offsetLeft;
-            dragoffset.y = e.pageY - element.offsetTop;
+            var elementClass = (e.target || e.srcElement).getAttribute('class');
+            if(elementClass !== "window-buttons" || elementClass !== "fas") {
+                isDragReady = true;
+                e.preventDefault();
+                //crossbrowser mouse pointer values
+                e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+                e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+                dragoffset.x = e.pageX - element.offsetLeft;
+                dragoffset.y = e.pageY - element.offsetTop;
+            }
         });
         _on(element.getElementsByClassName('modal-header')[0], 'mouseup', function () {
             //console.log("stopped dragging");
             isDragReady = false;
         });
-        _on(document, 'mousemove', function (e) {
+        _on(document.getElementById("workspace"), 'mousemove', function (e) {
 
             if (isDragReady) {
                 e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
@@ -127,6 +182,49 @@ function makeDraggable(id) {
                 element.style.left = offsetX + "px";
             }
         });
+        _on(element.getElementsByClassName('modal-header')[0], 'touchstart', function (e) {
+            var elementClass = (e.targetTouches[0].target || e.srcElement).getAttribute('class');
+            if(elementClass !== "window-buttons" || elementClass !== "fas") {
+                isDragReady = true;
+                e.preventDefault();
+                //crossbrowser mouse pointer values
+                e.touches[0].clientX = e.pageX || e.touches[0].clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+                e.touches[0].clientY = e.pageY || e.touches[0].clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+                dragoffset.x = e.touches[0].clientX - element.offsetLeft;
+                dragoffset.y = e.touches[0].clientY - element.offsetTop;
+            }
+        });
+        _on(element.getElementsByClassName('modal-header')[0], 'touchend', function () {
+            //console.log("stopped dragging");
+            isDragReady = false;
+        });
+        _on(element.getElementsByClassName('modal-header')[0], 'touchmove', function (e) {
+
+            if (isDragReady) {
+                e.touches[0].clientX = e.pageX || e.touches[0].clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+                e.touches[0].clientY = e.pageY || e.touches[0].clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+                // left/right constraint
+                if (e.touches[0].clientX - dragoffset.x < 0) {
+                    offsetX = 0;
+                } else if (e.touches[0].clientX - dragoffset.x + element.offsetWidth > document.body.clientWidth) {
+                    offsetX = document.body.clientWidth - element.offsetWidth;
+                } else {
+                    offsetX = e.touches[0].clientX - dragoffset.x;
+                }
+
+                // top/bottom constraint
+                if (e.touches[0].clientY - dragoffset.y < 40) {
+                    offsetY = 40;
+                } else if (e.touches[0].clientY - dragoffset.y + element.offsetHeight > document.body.clientHeight) {
+                    offsetY = document.body.clientHeight - element.offsetHeight;
+                } else {
+                    offsetY = e.touches[0].clientY - dragoffset.y;
+                }
+
+                element.style.top = offsetY + "px";
+                element.style.left = offsetX + "px";
+            }
+        });
     };
     //cross browser event Helper function
     var _on = function (element, event, functionality) {
@@ -136,7 +234,7 @@ function makeDraggable(id) {
 }
 
 //display date and time
-function funStartTime() {
+function startTime() {
   var now = new Date();
   var weekdayArray = new Array(7);
   weekdayArray[0] = "Sun";
@@ -171,7 +269,7 @@ function funStartTime() {
   var minute = now.getMinutes();
   minute = funCheckTime(minute);
   document.getElementById("time-date").innerHTML = day + " " + dayDate + " " + month + " " + year + " " + hour + ":" + minute;
-  var timeout = setTimeout(funStartTime, 500);
+  var timeout = setTimeout(startTime, 1000);
 }
 
 function funCheckTime(number) {
@@ -179,8 +277,8 @@ function funCheckTime(number) {
   return number;
 }
 
-function funOpenFullscreen() {
-  const element = document.body;
+function openFullscreen() {
+  const element = window.document.documentElement;
   var fullscreen;
   if (element.requestFullscreen) {
     fullscreen = element.requestFullscreen();
@@ -196,7 +294,7 @@ function funOpenFullscreen() {
 }
 
 /* Close fullscreen */
-function funCloseFullscreen() {
+function closeFullscreen() {
   var close;
   if (document.exitFullscreen) {
     close = document.exitFullscreen();
@@ -266,20 +364,34 @@ function makeResizableDiv(div) {
 }
 
 function expandShrinkWindow(div){
-
+    const header = document.querySelector(div + ' .modal-header');
     const element = document.querySelector(div + ' .modal-wrapper');
-
+    const container = document.querySelector(div + ' .modal-container');
     if(element.classList.contains("fullscreenWindow")) {
-        element.querySelector(".modal-container").style.height = ""
+        element.querySelector(".modal-container").style.height = "";
         element.classList.remove("fullscreenWindow");
         element.querySelector(".window-expand-shrink div").classList.remove("fa-compress");
         element.querySelector(".window-expand-shrink div").classList.add("fa-expand");
+        setTimeout(function() { container.classList.remove("resizing"); }, 10);
     } else {
+        container.classList.add("resizing");
         element.querySelector(".modal-container").style.height = (document.body.offsetHeight - 40) + "px"
         element.classList.add("fullscreenWindow")
         element.querySelector(".window-expand-shrink div").classList.remove("fa-expand");
         element.querySelector(".window-expand-shrink div").classList.add("fa-compress");
     }
+    element.querySelector(".modal-body").style.height = (container.offsetHeight - header.offsetHeight) + 'px'
+
+}
+
+function onMobileWindowModal(div) {
+    const header = document.querySelector(div + ' .modal-header');
+    const element = document.querySelector(div + ' .modal-container');
+    const container = document.querySelector('#workspace');
+
+    element.querySelector(".modal-body").style.height = (container.offsetHeight - header.offsetHeight) + 'px'
+    element.style.height = container.offsetHeight + 'px'
+
 }
 
 
