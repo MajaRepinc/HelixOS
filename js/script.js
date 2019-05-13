c407651a8c71f8a9c43d9934fd1be0626f46214a
Maja Repinc <maja.repinc96@gmail.com>
2019-05-14 00:20:12 +0100

    did fixes, added touch events on resize

--- js/script.js
+++ js/script.js
@@ -38,10 +38,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     document.addEventListener('keyup', function (event) {
-      if (event.defaultPrevented) { return; }
-      var key = event.key || event.keyCode;
+        if (event.defaultPrevented) { return; }
+        var key = event.key || event.keyCode;
 
-      if (key === 'F11' || key === "Escape") { keyFullscreen(key) }
+        if (key === 'F11' || key === "Escape") { keyFullscreen(key) }
     });
 
     //check for mobile browser
@@ -49,48 +49,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var check = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
         return check;
-    };
-    if(window.mobilecheck()) {
-        document.querySelector("body").setAttribute("class","mobile-view")
-    }
-
-
-});
+        };
+        if(window.mobilecheck()) {
+            document.querySelector("body").setAttribute("class","mobile-view")
+        }
+    });
 
 function hideContextMenu() {
     document.getElementById("context-menu").style.display = "none";
 }
 
 
-function loadingScreen(elementClass, typingSpeed){
+function loadingScreen(elementId, typingSpeed){
     var cursor = document.createElement("div");
     cursor.setAttribute("id", "loading-cursor")
-  var thhis = elementClass;
-  thhis.style.position =  "relative";
-  thhis.style.display =  "inline-block"
+    var currentElement = elementId;
+    currentElement.style.position =  "relative";
+    currentElement.style.display =  "inline-block"
 
-  thhis.append(cursor);
-  var loadCursor = document.getElementById("loading-cursor");
-  thhis = document.getElementById("loading-text");
-  var text = thhis.innerText.trim().split('');
-  var amntOfChars = text.length;
-  var newString = "";
-  thhis.innerText = "";
-  var loaded = false;
-  setTimeout(function(){
-    thhis.style.opacity = 1;
-    thhis.innerText = "";
-    for(var i = 0; i < amntOfChars; i++){
-      (function(i,char){
-        setTimeout(function() {
-          newString += char;
-          thhis.innerText = newString;
-        },i*typingSpeed);
-      })(i+1,text[i]);
-    }
-  },1500);
-
-
+    currentElement.append(cursor);
+    var loadCursor = document.getElementById("loading-cursor");
+    currentElement = document.getElementById("loading-text");
+    var text = currentElement.innerText.trim().split('');
+    var amntOfChars = text.length;
+    var newString = "";
+    currentElement.innerText = "";
+    var loaded = false;
+    setTimeout(function(){
+        currentElement.style.opacity = 1;
+        currentElement.innerText = "";
+        for(var i = 0; i < amntOfChars; i++){
+            (function(i,char){
+                setTimeout(function() {
+                    newString += char;
+                    currentElement.innerText = newString;
+                },i*typingSpeed);
+            })(i+1,text[i]);
+        }
+    },1500);
 }
 
 var themes = [
@@ -259,132 +255,141 @@ function makeDraggable(id) {
 
 //display date and time
 function startTime() {
-  var now = new Date();
-  var weekdayArray = new Array(7);
-  weekdayArray[0] = "Sun";
-  weekdayArray[1] = "Mon";
-  weekdayArray[2] = "Tue";
-  weekdayArray[3] = "Wed";
-  weekdayArray[4] = "Thu";
-  weekdayArray[5] = "Fri";
-  weekdayArray[6] = "Sat";
-  var day = weekdayArray[now.getDay()];
+    var now = new Date();
+    var weekdayArray = new Array(7);
+    weekdayArray[0] = "Sun";
+    weekdayArray[1] = "Mon";
+    weekdayArray[2] = "Tue";
+    weekdayArray[3] = "Wed";
+    weekdayArray[4] = "Thu";
+    weekdayArray[5] = "Fri";
+    weekdayArray[6] = "Sat";
+    var day = weekdayArray[now.getDay()];
 
-  var dayDate = now.getDate();
+    var dayDate = now.getDate();
 
-  var monthArray = new Array();
-  monthArray[0] = "Jan";
-  monthArray[1] = "Feb";
-  monthArray[2] = "Mar";
-  monthArray[3] = "Apr";
-  monthArray[4] = "May";
-  monthArray[5] = "Jun";
-  monthArray[6] = "Jul";
-  monthArray[7] = "Aug";
-  monthArray[8] = "Sep";
-  monthArray[9] = "Oct";
-  monthArray[10] = "Nov";
-  monthArray[11] = "Dec";
-  var month = monthArray[now.getMonth()];
+    var monthArray = new Array();
+    monthArray[0] = "Jan";
+    monthArray[1] = "Feb";
+    monthArray[2] = "Mar";
+    monthArray[3] = "Apr";
+    monthArray[4] = "May";
+    monthArray[5] = "Jun";
+    monthArray[6] = "Jul";
+    monthArray[7] = "Aug";
+    monthArray[8] = "Sep";
+    monthArray[9] = "Oct";
+    monthArray[10] = "Nov";
+    monthArray[11] = "Dec";
+    var month = monthArray[now.getMonth()];
 
-  var year = now.getFullYear();
+    var year = now.getFullYear();
 
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  minute = funCheckTime(minute);
-  document.getElementById("time-date").innerHTML = day + " " + dayDate + " " + month + " " + year + " " + hour + ":" + minute;
-  var timeout = setTimeout(startTime, 1000);
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    minute = funCheckTime(minute);
+    document.getElementById("time-date").innerHTML = day + " " + dayDate + " " + month + " " + year + " " + hour + ":" + minute;
+    var timeout = setTimeout(startTime, 1000);
 }
 
 function funCheckTime(number) {
-  if (number < 10) {number = "0" + number};  // add zero in front of numbers < 10
-  return number;
+      if (number < 10) {number = "0" + number};  // add zero in front of numbers < 10
+      return number;
 }
 
 function openFullscreen() {
-  const element = window.document.documentElement;
-  var fullscreen;
-  if (element.requestFullscreen) {
-    fullscreen = element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) { /* Firefox */
-    fullscreen = element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    fullscreen = element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) { /* IE/Edge */
-    fullscreen = element.msRequestFullscreen();
-  }
-
-  return fullscreen;
+    const element = window.document.documentElement;
+    var fullscreen;
+    if (element.requestFullscreen) {
+        fullscreen = element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { /* Firefox */
+        fullscreen = element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        fullscreen = element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { /* IE/Edge */
+        fullscreen = element.msRequestFullscreen();
+    }
+    return fullscreen;
 }
 
 /* Close fullscreen */
 function closeFullscreen() {
-  var close;
-  if (document.exitFullscreen) {
-    close = document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) { /* Firefox */
-    close = document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-    close = document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
-    close = document.msExitFullscreen();
-  }
-
-
-  return close;
-
+    var close;
+    if (document.exitFullscreen) {
+        close = document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        close = document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        close = document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        close = document.msExitFullscreen();
+    }
+    return close;
 }
 
 
 //Make the window module resizable
 function makeResizableDiv(div) {
-  const header = document.querySelector(div + ' .modal-header');
-  const element = document.querySelector(div + ' .modal-container');
-  const elementBody = element.querySelector('.modal-body')
-  const resizers = document.querySelectorAll(div + ' .resizer')
-  const minimumSize = 20;
-  let originalWidth = 0;
-  let originalHeight = 0;
-  let originalX = 0;
-  let originalY = 0;
-  let originalMouseX = 0;
-  let originalMouseY = 0;
-  elementBody.style.height = (element.offsetHeight - header.offsetHeight) + 'px'
-  for (let i = 0;i < resizers.length; i++) {
-    const currentResizer = resizers[i];
+    const header = document.querySelector(div + ' .modal-header');
+    const element = document.querySelector(div + ' .modal-container');
+    const elementBody = element.querySelector('.modal-body')
+    const resizers = document.querySelectorAll(div + ' .resizer')
+    const minimumSize = 20;
+    let originalWidth = 0;
+    let originalHeight = 0;
+    let originalX = 0;
+    let originalY = 0;
+    let originalMouseX = 0;
+    let originalMouseY = 0;
+    elementBody.style.height = (element.offsetHeight - header.offsetHeight) + 'px'
+    const currentResizer = resizers[0];
     currentResizer.addEventListener('mousedown', function(e) {
-      e.preventDefault()
-      originalWidth = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
-      originalHeight = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
-      originalX = element.getBoundingClientRect().left;
-      originalY = element.getBoundingClientRect().top;
-      originalMouseX = e.pageX;
-      originalMouseY = e.pageY;
-      element.classList.add("resizing");
-      window.addEventListener('mousemove', resize)
-      window.addEventListener('mouseup', stopResize)
-    })
+        e.preventDefault()
+        originalWidth = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+        originalHeight = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+        originalX = element.getBoundingClientRect().left;
+        originalY = element.getBoundingClientRect().top;
+        originalMouseX = e.pageX;
+        originalMouseY = e.pageY;
+        element.classList.add("resizing");
+        window.addEventListener('mousemove', resize)
+        window.addEventListener('mouseup', stopResize)
+    });
+
+    currentResizer.addEventListener('touchstart', function(e) {
+        e.preventDefault()
+        originalWidth = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+        originalHeight = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+        originalX = element.getBoundingClientRect().left;
+        originalY = element.getBoundingClientRect().top;
+        originalMouseX = e.pageX || e.touches[0].clientX;
+        originalMouseY = e.pageY || e.touches[0].clientY;
+        element.classList.add("resizing");
+        window.addEventListener('touchmove', resize)
+        window.addEventListener('touchend', stopResize)
+    });
 
     function resize(e) {
-      if (currentResizer.classList.contains('bottom-right')) {
-        const width = originalWidth + (e.pageX - originalMouseX);
-        const height = originalHeight + (e.pageY - originalMouseY)
-        if (width > minimumSize) {
-          element.style.width = width + 'px'
-
+        if (currentResizer.classList.contains('bottom-right')) {
+            var posX = e.pageX || e.touches[0].clientX;
+            var posY = e.pageY || e.touches[0].clientY;
+            const width = originalWidth + (posX - originalMouseX);
+            const height = originalHeight + (posY - originalMouseY)
+            if (width > minimumSize) {
+                element.style.width = width + 'px'
+            }
+            if (height > minimumSize) {
+                element.style.height =  height + 'px'
+                elementBody.style.height = (element.offsetHeight - header.offsetHeight) + 'px'
+            }
         }
-        if (height > minimumSize) {
-          element.style.height =  height + 'px'
-          elementBody.style.height = (element.offsetHeight - header.offsetHeight) + 'px'
-        }
-      }
     }
 
     function stopResize() {
-      window.removeEventListener('mousemove', resize)
-      element.classList.remove("resizing");
+        window.removeEventListener('mousemove', resize)
+        window.removeEventListener('touchmove', resize)
+        element.classList.remove("resizing");
     }
-  }
 }
 
 function expandShrinkWindow(div){
@@ -393,9 +398,9 @@ function expandShrinkWindow(div){
     const container = document.querySelector(div + ' .modal-container');
     if(element.classList.contains("fullscreenWindow")) {
         element.querySelector(".modal-container").style.height = "";
-        element.classList.remove("fullscreenWindow");
         element.querySelector(".window-expand-shrink div").classList.remove("fa-compress");
         element.querySelector(".window-expand-shrink div").classList.add("fa-expand");
+        element.classList.remove("fullscreenWindow");
         setTimeout(function() { container.classList.remove("resizing"); }, 10);
     } else {
         container.classList.add("resizing");
@@ -466,7 +471,6 @@ function checkColourSettings() {
 }
 
 function changeColorLuminance(hex, lum) {
-
 	// validate hex string
 	hex = String(hex).replace(/[^0-9a-f]/gi, '');
 	if (hex.length < 6) {
@@ -481,7 +485,6 @@ function changeColorLuminance(hex, lum) {
 		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
 		rgb += ("00"+c).substr(c.length);
 	}
-
 	return rgb;
 }
 
@@ -497,7 +500,6 @@ function focusWindow(element) {
 
 
 function changeHover() {
-
     var topBarElement =document.querySelectorAll('#top-bar .hover-element');
     for(var i=0;i<topBarElement.length;i++){
         topBarElement[i].addEventListener('mouseover',topBarHover,false);
@@ -514,7 +516,6 @@ function changeHover() {
 }
 
 function changeSearchHover() {
-
     var searchElement =document.querySelector('#search-modal .hover-element');
     searchElement.addEventListener('mouseover',searchHover,false);
 
@@ -530,7 +531,6 @@ function changeSearchHover() {
 }
 
 function changeContextMenuHover() {
-
     var topBarElement =document.querySelectorAll('#context-menu .hover-element');
     for(var i=0;i<topBarElement.length;i++){
         topBarElement[i].addEventListener('mouseover',contextMenuHover,false);
